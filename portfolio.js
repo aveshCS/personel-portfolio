8c42bc6689d0fa7189dfc2a971e92c395d7dc08a
Avesh Singh <aveshzoop@gmail.com>
2022-04-20 23:20:14 +0530

    setTimeout for ham

--- portfolio.js
+++ portfolio.js
@@ -5,8 +5,12 @@ document.querySelector(".ham").style.display="none";
 hamburger.addEventListener("click", ()=>{
     document.querySelector(".sidebar").classList.toggle("sidebarGo")
     if(document.querySelector(".sidebar").classList.contains("sidebarGo")){
-        document.querySelector(".ham").style.display="inline";
+        
         document.querySelector(".cross").style.display="none";
+        setTimeout(()=>{
+
+            document.querySelector(".ham").style.display="inline";
+        },200)
     }else{
         document.querySelector(".ham").style.display="none";
         setTimeout(()=>{
